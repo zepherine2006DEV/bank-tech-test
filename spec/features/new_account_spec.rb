f2512c9f78d83532c3581bb50d87e988821c96a2
James Hylins <james.hylins@johnlewis.co.uk>
2019-07-29 13:52:58 +0100

    add first feature test for initialising a new account

---
+++ spec/features/new_account_spec.rb
@@ -0,0 +1,8 @@
+describe 'new account feature' do
+  it 'should create a new account with initalised values' do
+    account = Account.new
+    expect(account.class).to eql(Account)
+    expect(account.balance).to eql(0)
+    expect(account.print_statement).to eql("date || credit || debit || balance")
+  end
+end
