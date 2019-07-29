d813c12a9eb0f0310befefa66bac505f8e8d538e
James Hylins <james.hylins@johnlewis.co.uk>
2019-07-29 14:35:49 +0100

    add a print_statement method to Account class

--- lib/account.rb
+++ lib/account.rb
@@ -5,4 +5,8 @@ class Account
   def initialize
     @balance = 0
   end
+
+  def print_statement
+    'date || credit || debit || balance'
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,10 @@ describe Account do
       expect(subject.balance).to eql(0)
     end
   end
+
+  describe '#print_statement' do
+    it 'should show headings only when account is first initialzed' do
+      expect(subject.print_statement).to eql('date || credit || debit || balance')
+    end
+  end
 end
