5ee0b6ad1a9c999d6021b5b9c2f17a1ed1969068
James Hylins <james.hylins@johnlewis.co.uk>
2019-07-30 11:15:03 +0100

    resolve linting errors

--- lib/account.rb
+++ lib/account.rb
@@ -8,25 +8,16 @@ class Account
 
   def print_statement
     statement_header = "date || credit || debit || balance\n"
+    
     statement_rows = @transactions.map do |transaction|
       [transaction[:date], transaction[:deposit], transaction[:withdrawal]]
-      end
-    totals = []
-    statement_rows.inject(0) do |sum, element|
-         new_total = sum + element[1] - element[2]
-         totals.push(new_total)
-         new_total
-       end
+    end
+    
+    totals = calculate_balances(statement_rows)
     statement_rows.map.with_index { |element, idx| element[3] = totals[idx] }
-    formatted_rows = statement_rows.map do |element|
-      "#{element[0].gsub('-','/')} " +
-      "||" +
-      (element[1] != 0 ? " #{element[1]}.00 " : " ") +
-      "||" +
-      (element[2] != 0 ? " #{element[2]}.00 " : " ") +
-      "||" +
-      " #{element[3]}.00"
-      end
+
+    formatted_rows = format_statement(statement_rows)
+    
     return statement_header + formatted_rows.reverse.join("\n")
   end
 
@@ -39,4 +30,29 @@ class Account
     transaction = { deposit: 0, withdrawal: amount, date: date }
     @transactions.push(transaction)
   end
+end
+
+private
+
+def calculate_balances(statement_rows)
+  totals = []
+  statement_rows.inject(0) do |sum, element|
+    new_total = sum + element[1] - element[2]
+    totals.push(new_total)
+    new_total
+  end
+  totals
+end
+
+def format_statement(statement_rows)
+  formatted_rows = statement_rows.map do |element|
+    "#{element[0].gsub('-', '/')} " +
+    "||" +
+    (element[1] != 0 ? " #{element[1]}.00 " : " ") +
+    "||" +
+    (element[2] != 0 ? " #{element[2]}.00 " : " ") +
+    "||" +
+    " #{element[3]}.00"
+  end
+  formatted_rows
 end
